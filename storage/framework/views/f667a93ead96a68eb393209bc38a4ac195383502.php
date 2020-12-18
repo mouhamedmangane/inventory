b703bf82dda1d1492679e9ed331521be6f1e29ad
sallousmane07 <ouz199607@gmail.com>
2020-12-18 12:01:31 +0100

    BD

---
+++ storage/framework/views/f667a93ead96a68eb393209bc38a4ac195383502.php
@@ -0,0 +1,3 @@
+<li>
+    <a href="<?php echo e($model->url); ?>"><?php echo e($model->name); ?></a>
+</li><?php /**PATH /var/www/html/PointDEVENTe/inventory/resources/views/components/sidebar/nav-item.blade.php ENDPATH**/ ?>
