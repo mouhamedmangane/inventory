bdd65916db6aacb547887395633b8774bece9c1a
mouhamedmangane <mouhamedmangane91@gmail.com>
2020-12-27 02:55:52 +0000

    user dropdown

--- resources/views/components/util/head-bar-action/bar.blade.php
+++ resources/views/components/util/head-bar-action/bar.blade.php
@@ -1,25 +1,68 @@
-<div class="header-content-1 d-flex align-items-center">
-    <div class="header-content-item pr-2 name-user">
-        <span class="text-normal  " >Test Nom</span>
-        <i class="material-icons-outlined ">expand_more</i>
-    </div>
+<div class="head-bar-action header-content-1 d-flex align-items-center overflow-auto" id="head-bar-action">
+
     <div class="header-content-item header-alert">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn" href="#notificationBar">
             <i class="material-icons-outlined ">notifications</i>
-        </button>
+        </a>
     </div>
     <div class="header-content-item header-param">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn">
             <i class="material-icons-outlined  " aria-hidden="true">brightness_7</i>
-        </button>
+        </a>
     </div>
     <div class="header-content-item header-info">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn">
             <i class="material-icons-outlined " aria-hidden="true">contact_support</i>
-        </button>
+        </a>
     </div>
-    <div class="header-content-item " style="padding:0px 12px;">
-        <img src="{{ URL::asset('images/profig.jpg') }}" alt="" width="30px" height="30px"  class="rounded-circle">
+    <div class="header-content-item-profil " >
+        <a href="#head-bar-action-user" class=" action-btn">
+            <img src="{{ URL::asset('images/profig.jpg') }}" alt="" width="32px" height="32px"  class="rounded-circle">
+        </a>
     </div>
-
+    
+    <div class="content">
+        <div class="close">
+            <i class="material-icons">close</i>
+        </div>
+        <x-util.head-bar-action.user-content />
+    </div>
 </div>
+
+@once
+    @push('script')
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#head-bar-action > .content > .close').on('click',function(){
+                    $('#head-bar-action .action-btn.active').removeClass('active');
+                    $('#head-bar-action > .content').toggleClass('active')
+                });
+                $('#head-bar-action').on('click','.action-btn',function(){
+                    if($('#head-bar-action .action-btn.active').is($(this))){
+                        $('#head-bar-action .action-btn.active').removeClass('active');
+                        $('#head-bar-action > .content').toggleClass('active')
+                    }
+                    else{
+                        if(! $('#head-bar-action > .content').hasClass('active')){
+                            $('#head-bar-action > .content').addClass('active');                        
+                        }
+
+                        // mettre a jour le button actif
+                        $('#head-bar-action .action-btn.active').removeClass('active');
+                        $(this).addClass('active');
+
+                        //mettre a jour le content actif
+                        var old_content = $('#head-bar-action .content-item.active');
+                        old_content.hide();
+                        old_content.removeClass('active');
+                        old_content.show();
+                        console.log($(this).attr('href'));
+                        let id=$(this).attr('href');
+                        $(id).addClass('active');
+                    }
+                    
+                });
+            });
+        </script>    
+    @endpush
+@endonce
--- storage/framework/views/81ccb94aa76c6b4e6e8ec81c10a75cd0331dfdc7.php
+++ storage/framework/views/81ccb94aa76c6b4e6e8ec81c10a75cd0331dfdc7.php
@@ -1,25 +1,79 @@
-<div class="header-content-1 d-flex align-items-center">
-    <div class="header-content-item pr-2 name-user">
-        <span class="text-normal  " >Test Nom</span>
-        <i class="material-icons-outlined ">expand_more</i>
-    </div>
+<div class="head-bar-action header-content-1 d-flex align-items-center overflow-auto" id="head-bar-action">
+
     <div class="header-content-item header-alert">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn" href="#notificationBar">
             <i class="material-icons-outlined ">notifications</i>
-        </button>
+        </a>
     </div>
     <div class="header-content-item header-param">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn">
             <i class="material-icons-outlined  " aria-hidden="true">brightness_7</i>
-        </button>
+        </a>
     </div>
     <div class="header-content-item header-info">
-        <button class="btn btn-default text-white pt-0">
+        <a class="nav-link action-btn">
             <i class="material-icons-outlined " aria-hidden="true">contact_support</i>
-        </button>
+        </a>
     </div>
-    <div class="header-content-item " style="padding:0px 12px;">
-        <img src="<?php echo e(URL::asset('images/profig.jpg')); ?>" alt="" width="30px" height="30px"  class="rounded-circle">
+    <div class="header-content-item-profil " >
+        <a href="#head-bar-action-user" class=" action-btn">
+            <img src="<?php echo e(URL::asset('images/profig.jpg')); ?>" alt="" width="32px" height="32px"  class="rounded-circle">
+        </a>
     </div>
+    
+    <div class="content">
+        <div class="close">
+            <i class="material-icons">close</i>
+        </div>
+         <?php if (isset($component)) { $__componentOriginala8c68ed9ac6ec9112d9ef337eac6bf20d644a3e9 = $component; } ?>
+<?php $component = $__env->getContainer()->make(App\View\Components\Util\HeadBarAction\UserContent::class, []); ?>
+<?php $component->withName('util.head-bar-action.user-content'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+<?php if (isset($__componentOriginala8c68ed9ac6ec9112d9ef337eac6bf20d644a3e9)): ?>
+<?php $component = $__componentOriginala8c68ed9ac6ec9112d9ef337eac6bf20d644a3e9; ?>
+<?php unset($__componentOriginala8c68ed9ac6ec9112d9ef337eac6bf20d644a3e9); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?> 
+    </div>
+</div>
 
-</div><?php /**PATH C:\noppal_workspace\laravel_project\inventory\resources\views/components/util/head-bar-action/bar.blade.php ENDPATH**/ ?>
+<?php if (! $__env->hasRenderedOnce('e204c844-da96-4893-9213-0b68d57d70ff')): $__env->markAsRenderedOnce('e204c844-da96-4893-9213-0b68d57d70ff'); ?>
+    <?php $__env->startPush('script'); ?>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#head-bar-action > .content > .close').on('click',function(){
+                    $('#head-bar-action .action-btn.active').removeClass('active');
+                    $('#head-bar-action > .content').toggleClass('active')
+                });
+                $('#head-bar-action').on('click','.action-btn',function(){
+                    if($('#head-bar-action .action-btn.active').is($(this))){
+                        $('#head-bar-action .action-btn.active').removeClass('active');
+                        $('#head-bar-action > .content').toggleClass('active')
+                    }
+                    else{
+                        if(! $('#head-bar-action > .content').hasClass('active')){
+                            $('#head-bar-action > .content').addClass('active');                        
+                        }
+
+                        // mettre a jour le button actif
+                        $('#head-bar-action .action-btn.active').removeClass('active');
+                        $(this).addClass('active');
+
+                        //mettre a jour le content actif
+                        var old_content = $('#head-bar-action .content-item.active');
+                        old_content.hide();
+                        old_content.removeClass('active');
+                        old_content.show();
+                        console.log($(this).attr('href'));
+                        let id=$(this).attr('href');
+                        $(id).addClass('active');
+                    }
+                    
+                });
+            });
+        </script>    
+    <?php $__env->stopPush(); ?>
+<?php endif; ?><?php /**PATH C:\noppal_workspace\laravel_project\inventory\resources\views/components/util/head-bar-action/bar.blade.php ENDPATH**/ ?>
