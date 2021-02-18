1e36e4266ac80bba3f38243af88f5560ceeb69a8
sallousmane07 <ouz199607@gmail.com>
2021-02-18 14:57:51 +0100

    c

--- .history/database/migrations/2020_11_29_235516_create_clients_table_20210118014107.php
+++ .history/database/migrations/2020_11_29_235516_create_clients_table_20210118014107.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,15 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::disableForeignKeyConstraints();
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 700);
+            $table->double('compte', 15, 2);
+            $table->string('ncni', 50);
+            $table->integer('tel')->unsigned()->nullable()->unique();
+            
 
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -29,6 +33,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('clients');
     }
 }
--- storage/framework/views/83188675ae01ec65db6c7144be06f42720ed42a6.php
+++
@@ -1,3 +0,0 @@
-
-
-<?php echo $__env->make('layouts.noppal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\noppal_workspace\laravel_project\inventory\resources\views/page/dashbord/dashboard.blade.php ENDPATH**/ ?>
